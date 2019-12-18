9c11a877961ac7a5852399ff02cc5d9971ccdbb4
victor <viheanaco@gmail.com>
2019-12-18 18:30:23 +0100

    socket-io: add namespaces #2

--- socket-io/server/index.js
+++ socket-io/server/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 server = app.listen(PORT);
 
+
 const io = socketIO(server);
 
 // the connection event is sent to the server whenever a client connects 
@@ -20,4 +21,26 @@ io.on('connection', socket => {
     // we can then also send events to the client using the emit method (custom events)
     socket.emit('welcome', "Welcome to this socket.io server");
     // The client also has to listen for the exact event we just sent
+});
+
+
+// USING NAMESPACES
+
+// we can use namespaces to create a form of seperation that act like groups
+// so from the beginning the clients can either connect to one namespace or the other
+
+// we use the io.of() method to declare a new namespace
+io.of('/games').on('connection', socket => {
+    console.log('new client connected to the games namespace with id: ', socket.id);
+
+    // you can use event names which are the same in other namespaces 
+    socket.emit('welcome', 'welcome to the games namespace')
+
+    // JOINING ROOMS
+    // we can use the events method to allow sockets join rooms
+    socket.on('joinRoom', room => {
+
+        // for the first person that tries to join this room, if it doesn't exist, that room is created
+        socket.join(room)
+    })
 });
